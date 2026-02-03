a75f0ad8aa235fb67f48e4075203defe24ec0585
Петър Ангелов Димитров <PADimitrov23@codingburgas.bg>
2026-02-03 14:35:38 +0200

    Remove unused variables and temp testing variables

--- LibraryManager/LibraryManager/main.cpp
+++ LibraryManager/LibraryManager/main.cpp
@@ -10,11 +10,6 @@ int main(){
     string title, author;
     float year, price;
 
-    BOOK book1{ "The Lord of the Rings", "J.R.R. Tolkien", 1954, 25.00 };
-    BOOK book2{ "Pride and Prejudice", "Jane Austen", 1813, 15.50 };
-    BOOK book3{ "1984", "George Orwell", 1949, 18.75 };
-
-
     LIBRARY myLibrary;
 
     while (true) {
